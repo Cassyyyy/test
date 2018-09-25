64973267a0a2043aa70b79bd0e26735d49f3aeb0
Cassyyyy <43379632+Cassyyyy@users.noreply.github.com>
2018-09-25 15:52:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("THANOS CAR!");
+        System.out.println("THANOS CAR");
+        int OwOwaarde = 17;
+            OwOwaarde = 17+1;
+            System.out.print (OwOwaarde);
+
+            boolean your_mommi_big_gay = true;
+                your_mommi_big_gay = false;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("is your mami gay?");
+        OwOwaarde = scanner.nextInt();
+    }
+}
+
